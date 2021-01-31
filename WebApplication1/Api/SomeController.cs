b533f4dfc9f68e8d01e12df2a5b5a4109fcd443a
Adamantica <adamantica@protonmail.com>
2021-01-31 07:29:22 +0200

    Repro

---
+++ WebApplication1/Api/SomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SomeController : ControllerBase
+    {
+        [HttpPost]
+        public string Post([FromForm] string parameter)
+        {
+            // Does not work if parameter is an empty string.
+
+            Request.Form.TryGetValue("parameter", out var value);
+            var strValue = (string)value;
+
+            return $"parameter type: {parameter.GetType().ToString()}, strValue type: {strValue.GetType().ToString()}";
+        }
+    }
+}
---
+++ WebApplication1/Api/SomeOtherController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SomeOtherController : ControllerBase
+    {
+        [HttpPost]
+        public string Post([FromForm] string? parameter)
+        {
+            // Does work if parameter is an empty string, but then it will be interpreted as null.
+
+            Request.Form.TryGetValue("parameter", out var value);
+            var strValue = (string)value;
+
+            if (parameter == null) 
+            {
+                return $"parameter type: null, strValue type: {strValue.GetType().ToString()}";
+
+            }
+
+            return $"parameter type: {parameter.GetType().ToString()}, strValue type: {strValue.GetType().ToString()}";
+
+        }
+    }
+}
